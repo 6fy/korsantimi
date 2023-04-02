7f41096370efc1933e693ecc71702db59dc3d79a
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2023-04-02 17:16:10 +0300

    add: trendShip

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import Layout from "@/layout/Layout";
 import Hero from "@/components/hero/Hero";
+import TrendShip from "@/components/trendShip/TrendShip";
 
 export default function Home() {
   return (
     <Layout>
       <Hero />
+      <TrendShip />
     </Layout>
   );
 }
