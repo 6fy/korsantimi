4a8274b30c9d7ce3914388832a2774d3380d34df
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2023-04-02 17:20:36 +0300

    add: banner

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 import Layout from "@/layout/Layout";
 import Hero from "@/components/hero/Hero";
 import TrendShip from "@/components/trendShip/TrendShip";
+import Banner from "@/components/banner/Banner";
 
 export default function Home() {
   return (
     <Layout>
       <Hero />
       <TrendShip />
+      <Banner title="Lorem Ipsum is simply dummy text of the printing and typesetting industry." />
     </Layout>
   );
 }
