612e657b782f8b797ef4987a8fdeddaf66f2b927
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2023-04-02 16:39:45 +0300

    add: layout

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -1,10 +1,17 @@
 import type { FC, ReactNode } from "react";
+import styles from "./Layout.module.css";
+import Navbar from "@/components/navbar/Navbar";
 
 type IProps = {
   children: ReactNode;
 };
 const Layout: FC<IProps> = ({ children }) => {
-  return <div>{children}</div>;
+  return (
+    <div className={styles.container}>
+      <Navbar />
+      <div className={styles.content}>{children}</div>
+    </div>
+  );
 };
 
 export default Layout;
