25fab06f4b187d1d3c93fa260e8e7ea58fca7a47
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2023-04-03 23:03:54 +0300

    add: contact

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Layout from "@/layout/Layout";
 import Hero from "@/components/hero/Hero";
 import TrendShip from "@/components/trendShip/TrendShip";
 import Banner from "@/components/banner/Banner";
+import Contact from "@/components/contact/Contact";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <Hero />
       <TrendShip />
       <Banner title="Lorem Ipsum is simply dummy text of the printing and typesetting industry." />
+      <Contact />
     </Layout>
   );
 }
